dfff85b4b3f255ebe3ba3c81754f4eee12c452d1
matiss <matissjurevics@gmail.com>
2024-03-25 18:04:55 +0000

    fully implemented getVCollision

--- java/src/C22501743/raycaster.java
+++ java/src/C22501743/raycaster.java
@@ -38,26 +38,68 @@ public class Raycaster {
     colors.put("floor", "#f4a460");
     colors.put("ceiling", "#44f");
 
-    public boolean outOfBounds(double x, double y) {
+    public boolean outOfBounds(int x, int y) {
         return x < 0 || x > map[0].length || y < 0 || y > map.length;
     }
 
     public static double toRadians(int degrees) {
         return degrees * (180 / PApplet.PI);
     }
+    public static double distance(double x1,double y1,double x2,double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); // Just uses pythogoras to get the distance between two points
+    }
 
-    public class ray {
+    public class Ray {
         double angle;
         double distance;
         boolean vertical;
     }
 
-    public ray getVCollision(double angle) {
-        static double right = Math.abs(Math.floor((angle - Math.PI / 2) / Math.PI) % 2) // Checks if the camera is facing the right
-        static double firstX;
+    public Ray getVCollision(double angle) {
+        boolean right = Math.abs(Math.floor((angle - Math.PI / 2) / Math.PI) % 2) == 0 ? false : true; // Checks if the camera is facing the right
+        double firstX, firstY;
+
         if (right) {
             firstX = Math.floor(c.x / CELL_SIZE) * CELL_SIZE + CELL_SIZE;
+        } else {
+            firstX = Math.floor(c.x / CELL_SIZE) * CELL_SIZE;
         }
+
+        firstY = c.y + (firstX - c.x) * Math.tan(angle);
+
+        int xA = right ? CELL_SIZE : -CELL_SIZE;
+        double yA = xA * Math.tan(angle);
+
+        boolean wall = false;
+        double nextX = firstX;
+        double nextY = firstY;
+        int cellX, cellY;
+
+        while (!wall) {
+            cellX = right ? (int) Math.floor(nextX / CELL_SIZE) : (int) Math.floor(nextX/CELL_SIZE) - 1;
+            cellY = (int) Math.floor(nextY / CELL_SIZE);
+            if (outOfBounds(cellX, cellY)) {
+                break;
+            }
+            wall = map[cellY][cellX] == 1 ? true : false;
+            if (!wall) {
+                nextX += xA;
+                nextY += yA;
+            }
+        }
+        
+        // Creates a new ray to be returned
+        Ray returnVal;
+        returnVal.distance = distance(c.x, c.y, nextX, nextY);
+        returnVal.vertical = true;
+        returnVal.angle = angle;
+
+        return returnVal;
+    }
+
+    public Ray getHCollision(double angle) {
+        boolean up = Math.abs(Math.floor((angle / Math.PI) % 2)) != 0 ? true : false;
+        
     }
     
    
