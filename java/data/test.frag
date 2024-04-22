382fbc44f1e2d3d54f163391b1228c112d863de1
matiss <matissjurevics@gmail.com>
2024-04-22 16:08:52 +0100

    Guitar hero lol

--- java/data/test.frag
+++ java/data/test.frag
@@ -1,8 +1,48 @@
-void main() {
-  // Just make it white
-  gl_FragColor = vec4(1., 1., 0.5, 1);
-}
 
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+uniform float u_circleRadius;
+
+vec3 palette( float t ) {
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(0.1843, 0.0, 1.0);
+    vec3 d = vec3(0.2627, 0.5176, 0.5569);
+
+    return a + b*cos( 6.28318*(c*t+d) );
+}
+void main() {
+
+    vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution.xy) / u_resolution.y;
+    vec2 uv0 = uv;
+    vec3 finalColor = vec3(0.0);
+    
+    for (float i = 0.0; i < 4.0; i++) {
+        uv = fract(uv * 1.5) - 0.5;
+
+        float d = length(uv) * exp(-length(uv0));
+
+        vec3 col = palette(length(uv0) + i*.4 + u_time*0.8);
+
+        d = sin(d*8. + u_time)/4.;
+        d = abs(d);
+
+        // d = pow(0.01 / d, 1.2);
+        
+
+        finalColor += col * d;
+    }
+    if (length(uv0) < u_circleRadius + 0.1) {
+        finalColor = vec3(0.0);
+    }
+        
+    gl_FragColor = vec4(finalColor, 1.0);
+}
 
 
 
--- java/src/C22501743/MyVisual.java
+++ java/src/C22501743/MyVisual.java
@@ -1,5 +1,6 @@
 package C22501743;
 import ie.tudublin.Visual;
+import processing.opengl.PShader;
 public class MyVisual extends Visual {
     int rectsPerLine = 60;
     float[][] rectCoords = new float[rectsPerLine][2];
@@ -7,10 +8,11 @@ public class MyVisual extends Visual {
     float[] endPoints = new float[2];       // x, y values for the end point of a line that the rectangles will be drawn on
 
     public void settings() {
-        size(1920, 1080);
+        size(1920, 1080, P2D);
     }
 
     public void setup() {
+        frameRate(60);
         startMinim();
         loadAudio("heroplanet.mp3");
         colorMode(HSB);
@@ -19,29 +21,42 @@ public class MyVisual extends Visual {
         endPoints[0] = width/2;
         endPoints[1] = height/2;
     }
-
+    PShader testShader;
     public void draw() {
-        // calculateAverageAmplitude();
-        background(0);
-        // stroke(255);
-        int rectSize = 60;
+        drawBackground();
+        testShader = loadShader("test.frag");
+        shader(testShader);
+        float halfHeight = height/2;
+        testShader.set("u_resolution", (float)width, (float)halfHeight);
+        testShader.set("u_time", millis()/1000.0f);
+        rect(0,0,width,height);
+        drawGround();
 
-        for (int i = 0; i < rectsPerLine; i++) {
-            rectCoords[i][0] = map(i, 0, rectsPerLine, startPoints[0], endPoints[0]);
-            rectCoords[i][1] = map(i, 0, rectsPerLine, startPoints[1], endPoints[1]);
+    }
+
+    public void drawGround() {
+        resetShader();
+        fill(0, 200f);
+        quad((width*0.25f), height/2, (width*0.75f), height/2, (width*0.9167f), height, (width*0.0833f), height);
+        int startHeight = height/2;
+        int endHeight = height;
+        int lineNum = 15;
+        for (int i = 0; i < lineNum; i++) {
+            float ln2 = lineNum * lineNum;
+            float i2 = i * i;
+            float curHeight = map(i2, 0, ln2, startHeight, endHeight);
+            stroke(255);
+            float startWidth = map(i2, 0, ln2, 0.25f, 0.0833f) * width;
+            float endWidth = map(i2, 0, ln2, 0.75f, 0.9167f) * width;
+            line(startWidth, curHeight, endWidth, curHeight);
         }
-        int i = 0;
-        
-        while ( i < rectsPerLine) {
-            float c = map(i, 0, rectsPerLine, 0, 255);
-            fill(c, 255, 255, map(i, 0, rectsPerLine, 20, 200));
-            // print a rect once every 0.5 seconds
-            if (millis() % 500 < 10) {
-                rect(rectCoords[i][0], rectCoords[i][1], rectSize, rectSize);
-                i++;
-            }
-            
-        }
+        line((3*width)/4, startHeight, (11*width)/12, endHeight);
+        line((width)/4, startHeight, (1*width)/12, endHeight);
+    }
+    public void drawBackground() {
+        background(0);
+        stroke(255);
+        resetShader();
         
     }
 }
