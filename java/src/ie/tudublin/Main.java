0485ec649ca99c63868b19bc0f200c196ea164fe
matiss <matissjurevics@gmail.com>
2024-04-24 14:32:20 +0100

    Revert "updated the main.java file"
    This reverts commit 1bcbd07fca74574697d7434e7f8975bba8eaf793.

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -6,19 +6,12 @@ package ie.tudublin;
 // import example.RotatingAudioBands;
 import C22386123.myVisual1;
 import C22501743.EyeVisual;
-import processing.core.PApplet;
 
 public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        PApplet eye;
-        eye = new EyeVisual();
-        PApplet myVisual1;
-        myVisual1 = new myVisual1();
-
-        PApplet.runSketch(a, eye);
-        
+        processing.core.PApplet.runSketch(a, new EyeVisual());
     }
 
     public static void main(String[] args) {
