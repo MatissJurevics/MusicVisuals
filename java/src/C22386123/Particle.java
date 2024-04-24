2c75408671a37abe906daa8cd8ef1afc5f939aea
Karl Negrillo <c22386123@mytudublin.ie>
2024-04-24 14:41:44 +0100

    added a bit more, changed particles

--- java/src/C22386123/Particle.java
+++ java/src/C22386123/Particle.java
@@ -9,22 +9,26 @@ public class Particle {
     PVector velocity;
     float lifespan;
 
+     // Constructor initializing a particle with a position.
     Particle(PApplet sketch, float x, float y) {
         this.sketch = sketch;
-        position = new PVector(x, y, 0);
+        position = new PVector(x, y, 0); // Position in 2D space.
         velocity = new PVector(sketch.random(-1, 1), sketch.random(1, 3));
         lifespan = 255;
     }
 
+    // Update the position and decrease the lifespan of the particle.
     void update() {
         position.add(velocity);
         lifespan -= 2;
     }
 
+    // Check if the particle's lifespan has ended.
     boolean isDead() {
         return lifespan < 0;
     }
 
+    // Display the particle on the canvas.
     void display() {
         sketch.noStroke();
         sketch.fill(255, lifespan);
